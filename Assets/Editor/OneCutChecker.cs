e176de43e3c2c615acfb9938dd42ea84e0bf74b5
DESKTOP-MBK0LP5\ringo <mail@ynoringo.com>
2018-05-14 02:23:04 +0900

    upload

---
+++ Assets/Editor/OneCutChecker.cs
@@ -0,0 +1,184 @@
+﻿using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+public class OneCutChecker : EditorWindow
+{
+    bool isPlayingLoop;
+    int speed = 60;
+
+    static OneCutChecker oneCutChecker;
+    static ScreenController screenController;
+    static EditorWindow gameView;
+    static bool useShortcutKey = true;
+
+    [MenuItem("Window/One Cut Checker")]
+    static void ShowWindow()
+    {
+        if (oneCutChecker == null)
+        {
+            oneCutChecker = GetWindow<OneCutChecker>();
+            oneCutChecker.titleContent = new GUIContent("One Cut Checker");
+            oneCutChecker.Show();
+        }
+    }
+
+    [MenuItem("Edit/Pause _p")]
+    static void Pause()
+    {
+        if (!useShortcutKey)
+        {
+            return;
+        }
+
+        EditorApplication.isPaused = !EditorApplication.isPaused;
+    }
+
+    [MenuItem("Edit/Previous frame _LEFT")]
+    static void PrevFrame()
+    {
+        if (!useShortcutKey || screenController == null || screenController.FramePosition <= 0)
+        {
+            return;
+        }
+
+        screenController.FramePosition--;
+        gameView.Repaint();
+    }
+
+    [MenuItem("Edit/Next frame _RIGHT")]
+    static void NextFrame()
+    {
+        if (!useShortcutKey)
+        {
+            return;
+        }
+
+        if (screenController != null && screenController.FramePosition < screenController.Texture2Ds.Count - 1)
+        {
+            screenController.FramePosition++;
+            gameView.Repaint();
+        }
+        else
+        {
+            EditorApplication.Step();
+        }
+    }
+
+    void OnEnable()
+    {
+        if (gameView == null)
+        {
+            var assembly = typeof(EditorWindow).Assembly;
+            var type = assembly.GetType("UnityEditor.GameView");
+            gameView = GetWindow(type);
+        }
+    }
+
+    void OnGUI()
+    {
+        DoToolbar();
+
+        EditorGUILayout.LabelField("Shortcut key");
+
+        EditorGUILayout.BeginVertical("box");
+
+        var style = new GUIStyle(GUI.skin.label);
+        style.wordWrap = true;
+        EditorGUILayout.LabelField("p  : Pause/Resume\n← : Previous frame\n→ : Next frame", style);
+
+        EditorGUILayout.EndVertical();
+
+        useShortcutKey = EditorGUILayout.Toggle("Use shortcut key", useShortcutKey);
+
+        if (!EditorApplication.isPaused || screenController == null)
+        {
+            return;
+        }
+
+        if (screenController.Texture2Ds.Count > 1)
+        {
+            EditorGUILayout.Space();
+
+            screenController.FramePosition = EditorGUILayout.IntSlider("Frame position", screenController.FramePosition, 0, screenController.Texture2Ds.Count - 1);
+            gameView.Repaint();
+
+            EditorGUILayout.Space();
+
+            if (isPlayingLoop)
+            {
+                speed = EditorGUILayout.IntSlider("Speed (FPS)", speed, 1, 150);
+                gameView.Repaint();
+
+                EditorGUILayout.Space();
+            }
+
+            if (GUILayout.Button(isPlayingLoop ? "Pause loop" : "Play loop"))
+            {
+                isPlayingLoop = !isPlayingLoop;
+
+                if (isPlayingLoop)
+                {
+                    PlayLoop();
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            screenController = null;
+            isPlayingLoop = false;
+
+            return;
+        }
+
+        if (screenController == null)
+        {
+            screenController = new GameObject("Screen Controller").AddComponent<ScreenController>();
+        }
+
+        Repaint();
+
+        if (!EditorApplication.isPaused)
+        {
+            isPlayingLoop = false;
+
+            return;
+        }
+    }
+
+    void DoToolbar()
+    {
+        GUILayout.BeginHorizontal(EditorStyles.toolbar);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+
+    void PlayLoop()
+    {
+        speed = Application.targetFrameRate == -1 ? 60 : Application.targetFrameRate;
+
+        var thread = new Thread(() =>
+        {
+            while (isPlayingLoop)
+            {
+                if (screenController.FramePosition == screenController.Texture2Ds.Count - 1)
+                {
+                    screenController.FramePosition = 0;
+                }
+                else
+                {
+                    screenController.FramePosition++;
+                }
+
+                Repaint();
+
+                Thread.Sleep(Mathf.RoundToInt(1.0f / speed * 1000.0f));
+            }
+        });
+        thread.Start();
+    }
+}
---
+++ Assets/Scripts/ScreenController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+    [HideInInspector]
+    public List<Texture2D> Texture2Ds = new List<Texture2D>();
+    [HideInInspector]
+    public int FramePosition;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (!EditorApplication.isPaused)
+        {
+            FramePosition = -1;
+
+            if (Texture2Ds.Count > 0)
+            {
+                Texture2Ds.Clear();
+            }
+
+            return;
+        }
+
+        StartCoroutine(CaptureScreen());
+    }
+
+    void OnGUI()
+    {
+        if (!EditorApplication.isPaused || Texture2Ds.Count == 0)
+        {
+            return;
+        }
+
+        if (FramePosition + 1 != Texture2Ds.Count)
+        {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Texture2Ds[FramePosition], ScaleMode.ScaleToFit, false);
+        }
+    }
+
+    IEnumerator CaptureScreen()
+    {
+        FramePosition = Mathf.Max(Texture2Ds.Count - 1, 0);
+
+        yield return new WaitForEndOfFrame();
+
+        var texture2D = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        texture2D.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        texture2D.Apply();
+        Texture2Ds.Add(texture2D);
+
+        FramePosition = Texture2Ds.Count - 1;
+    }
+}
