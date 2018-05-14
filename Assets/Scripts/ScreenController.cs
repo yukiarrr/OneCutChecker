b1ae3f2c18de12349ec1e3041adcbc410e8215e7
DESKTOP-MBK0LP5\ringo <mail@ynoringo.com>
2018-05-15 02:46:17 +0900

    add #if UNITY_EDITOR

--- Assets/Scripts/ScreenController.cs
+++ Assets/Scripts/ScreenController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -58,3 +59,4 @@ public class ScreenController : MonoBehaviour
         FramePosition = Texture2Ds.Count - 1;
     }
 }
+#endif
